bcbda23bd7f1a3bc04659878a8b87b6c7141cac2
KOH WEI LONG DYLAN TP <1907446G@student.tp.edu.sg>
2020-01-02 21:09:30 +0800

    New commit but needs to change

--- Session6/InventoryDashboard.Designer.cs
+++ Session6/InventoryDashboard.Designer.cs
@@ -28,6 +28,12 @@
         /// </summary>
         private void InitializeComponent()
         {
+            System.Windows.Forms.DataVisualization.Charting.ChartArea chartArea1 = new System.Windows.Forms.DataVisualization.Charting.ChartArea();
+            System.Windows.Forms.DataVisualization.Charting.Legend legend1 = new System.Windows.Forms.DataVisualization.Charting.Legend();
+            System.Windows.Forms.DataVisualization.Charting.Series series1 = new System.Windows.Forms.DataVisualization.Charting.Series();
+            System.Windows.Forms.DataVisualization.Charting.ChartArea chartArea2 = new System.Windows.Forms.DataVisualization.Charting.ChartArea();
+            System.Windows.Forms.DataVisualization.Charting.Legend legend2 = new System.Windows.Forms.DataVisualization.Charting.Legend();
+            System.Windows.Forms.DataVisualization.Charting.Series series2 = new System.Windows.Forms.DataVisualization.Charting.Series();
             this.groupBox1 = new System.Windows.Forms.GroupBox();
             this.groupBox2 = new System.Windows.Forms.GroupBox();
             this.groupBox3 = new System.Windows.Forms.GroupBox();
@@ -36,10 +42,26 @@
             this.inventCBtn = new System.Windows.Forms.Button();
             this.closeBtn = new System.Windows.Forms.Button();
             this.languageBoc = new System.Windows.Forms.ComboBox();
+            this.spendList = new System.Windows.Forms.DataGridView();
+            this.mostUsedList = new System.Windows.Forms.DataGridView();
+            this.costlyAssetList = new System.Windows.Forms.DataGridView();
+            this.pieChart = new System.Windows.Forms.DataVisualization.Charting.Chart();
+            this.barChart = new System.Windows.Forms.DataVisualization.Charting.Chart();
+            this.groupBox1.SuspendLayout();
+            this.groupBox2.SuspendLayout();
+            this.groupBox3.SuspendLayout();
+            this.groupBox4.SuspendLayout();
+            this.groupBox5.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.spendList)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.mostUsedList)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.costlyAssetList)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pieChart)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.barChart)).BeginInit();
             this.SuspendLayout();
             // 
             // groupBox1
             // 
+            this.groupBox1.Controls.Add(this.spendList);
             this.groupBox1.Location = new System.Drawing.Point(12, 12);
             this.groupBox1.Name = "groupBox1";
             this.groupBox1.Size = new System.Drawing.Size(751, 142);
@@ -49,6 +71,7 @@
             // 
             // groupBox2
             // 
+            this.groupBox2.Controls.Add(this.pieChart);
             this.groupBox2.Location = new System.Drawing.Point(769, 12);
             this.groupBox2.Name = "groupBox2";
             this.groupBox2.Size = new System.Drawing.Size(436, 247);
@@ -58,6 +81,7 @@
             // 
             // groupBox3
             // 
+            this.groupBox3.Controls.Add(this.barChart);
             this.groupBox3.Location = new System.Drawing.Point(769, 265);
             this.groupBox3.Name = "groupBox3";
             this.groupBox3.Size = new System.Drawing.Size(436, 231);
@@ -68,6 +92,7 @@
             // 
             // groupBox4
             // 
+            this.groupBox4.Controls.Add(this.mostUsedList);
             this.groupBox4.Location = new System.Drawing.Point(12, 160);
             this.groupBox4.Name = "groupBox4";
             this.groupBox4.Size = new System.Drawing.Size(751, 142);
@@ -77,6 +102,7 @@
             // 
             // groupBox5
             // 
+            this.groupBox5.Controls.Add(this.costlyAssetList);
             this.groupBox5.Location = new System.Drawing.Point(12, 308);
             this.groupBox5.Name = "groupBox5";
             this.groupBox5.Size = new System.Drawing.Size(751, 142);
@@ -112,6 +138,69 @@
             this.languageBoc.Size = new System.Drawing.Size(121, 21);
             this.languageBoc.TabIndex = 7;
             // 
+            // spendList
+            // 
+            this.spendList.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.spendList.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.spendList.Location = new System.Drawing.Point(3, 16);
+            this.spendList.Name = "spendList";
+            this.spendList.Size = new System.Drawing.Size(745, 123);
+            this.spendList.TabIndex = 0;
+            // 
+            // mostUsedList
+            // 
+            this.mostUsedList.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.mostUsedList.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.mostUsedList.Location = new System.Drawing.Point(3, 16);
+            this.mostUsedList.Name = "mostUsedList";
+            this.mostUsedList.Size = new System.Drawing.Size(745, 123);
+            this.mostUsedList.TabIndex = 0;
+            // 
+            // costlyAssetList
+            // 
+            this.costlyAssetList.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.costlyAssetList.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.costlyAssetList.Location = new System.Drawing.Point(3, 16);
+            this.costlyAssetList.Name = "costlyAssetList";
+            this.costlyAssetList.Size = new System.Drawing.Size(745, 123);
+            this.costlyAssetList.TabIndex = 0;
+            // 
+            // pieChart
+            // 
+            chartArea1.Name = "ChartArea1";
+            this.pieChart.ChartAreas.Add(chartArea1);
+            this.pieChart.Dock = System.Windows.Forms.DockStyle.Fill;
+            legend1.Name = "Legend1";
+            this.pieChart.Legends.Add(legend1);
+            this.pieChart.Location = new System.Drawing.Point(3, 16);
+            this.pieChart.Name = "pieChart";
+            this.pieChart.Palette = System.Windows.Forms.DataVisualization.Charting.ChartColorPalette.None;
+            series1.ChartArea = "ChartArea1";
+            series1.ChartType = System.Windows.Forms.DataVisualization.Charting.SeriesChartType.Pie;
+            series1.Legend = "Legend1";
+            series1.Name = "Series1";
+            this.pieChart.Series.Add(series1);
+            this.pieChart.Size = new System.Drawing.Size(430, 228);
+            this.pieChart.TabIndex = 0;
+            this.pieChart.Text = "chart1";
+            // 
+            // barChart
+            // 
+            chartArea2.Name = "ChartArea1";
+            this.barChart.ChartAreas.Add(chartArea2);
+            this.barChart.Dock = System.Windows.Forms.DockStyle.Fill;
+            legend2.Name = "Legend1";
+            this.barChart.Legends.Add(legend2);
+            this.barChart.Location = new System.Drawing.Point(3, 16);
+            this.barChart.Name = "barChart";
+            series2.ChartArea = "ChartArea1";
+            series2.Legend = "Legend1";
+            series2.Name = "Series1";
+            this.barChart.Series.Add(series2);
+            this.barChart.Size = new System.Drawing.Size(430, 212);
+            this.barChart.TabIndex = 0;
+            this.barChart.Text = "chart1";
+            // 
             // InventoryDashboard
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
@@ -127,6 +216,17 @@
             this.Controls.Add(this.groupBox1);
             this.Name = "InventoryDashboard";
             this.Text = "Inventory Dashboard";
+            this.Load += new System.EventHandler(this.InventoryDashboard_Load);
+            this.groupBox1.ResumeLayout(false);
+            this.groupBox2.ResumeLayout(false);
+            this.groupBox3.ResumeLayout(false);
+            this.groupBox4.ResumeLayout(false);
+            this.groupBox5.ResumeLayout(false);
+            ((System.ComponentModel.ISupportInitialize)(this.spendList)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.mostUsedList)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.costlyAssetList)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pieChart)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.barChart)).EndInit();
             this.ResumeLayout(false);
 
         }
@@ -141,6 +241,11 @@
         private System.Windows.Forms.Button inventCBtn;
         private System.Windows.Forms.Button closeBtn;
         private System.Windows.Forms.ComboBox languageBoc;
+        private System.Windows.Forms.DataGridView spendList;
+        private System.Windows.Forms.DataVisualization.Charting.Chart pieChart;
+        private System.Windows.Forms.DataVisualization.Charting.Chart barChart;
+        private System.Windows.Forms.DataGridView mostUsedList;
+        private System.Windows.Forms.DataGridView costlyAssetList;
     }
 }
 
--- Session6/InventoryDashboard.cs
+++ Session6/InventoryDashboard.cs
@@ -26,5 +26,70 @@ namespace Session6
         {
             Close();
         }
+
+        private void InventoryDashboard_Load(object sender, EventArgs e)
+        {
+            GridRefresh();
+        }
+
+        private void GridRefresh()
+        {
+            spendList.ColumnCount = 1;
+            spendList.Columns[0].Name = "Department / Month";
+            using (var context = new Session6Entities())
+            {
+                var getDepartment = (from x in context.Departments
+                                     select x.Name).ToList().Distinct();
+
+
+                var getDepartmentSpendingGrouping = (from x in context.Orders
+                                                     where x.EmergencyMaintenance.EMEndDate != null
+                                                     orderby x.Date descending
+                                                     select new { intDate = x.Date }).ToList();
+
+                var getDistinctDates = (from x in getDepartmentSpendingGrouping
+                                        select new { date = x.intDate.ToString("yyyy-MM") }).ToList();
+                List<string> department = new List<string>();
+
+
+                foreach (var item1 in getDistinctDates.Select(x => x.date).Distinct().Take(10))
+                {
+                    spendList.Columns.Add(item1, item1);
+
+                    foreach (var departments in getDepartment)
+                    {
+
+                        var getDetailsOfSpending = (from x in context.Orders
+                                                    select x).ToList();
+
+                        var dID = (from x in context.Departments
+                                   where x.Name.Equals(departments)
+                                   select x.ID).First();
+
+                        var getDetailsOfSpending1 = (from x in getDetailsOfSpending
+                                                     where x.Date.ToString("yyyy-MM") == item1
+                                                     join y in context.EmergencyMaintenances on x.EmergencyMaintenancesID equals y.ID
+                                                     where y.Asset.DepartmentLocation.DepartmentID.Equals(dID)
+                                                     select new { spending = x.OrderItems.Sum(p => p.Amount * p.UnitPrice) });
+
+                        var getDetailsOfSpending2 = (from x in getDetailsOfSpending1
+                                                     select x.spending);
+                        // TO CHANGE, F'ed up the DGV
+                        foreach (var spendings in getDetailsOfSpending)
+                        {
+                            List<string> vs = new List<string>()
+                                {
+                                    departments, spendings.ToString(), 
+                                };
+                            spendList.Rows.Add(vs.ToArray());
+                        }
+
+
+                    }
+                }
+
+
+            }
+        }
     }
 }
