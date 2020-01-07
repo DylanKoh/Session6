a07d47b06607bb00dcb1c3d2a81b695a28158656
KOH WEI LONG DYLAN TP <1907446G@student.tp.edu.sg>
2020-01-07 09:30:52 +0800

    huh? checking commit lor

--- Session6/InventoryControl.cs
+++ Session6/InventoryControl.cs
@@ -12,6 +12,7 @@ namespace Session6
 {
     public partial class InventoryControl : Form
     {
+        // Check Commit
         public InventoryControl()
         {
             InitializeComponent();
@@ -30,6 +31,36 @@ namespace Session6
         private void cancelBtn_Click(object sender, EventArgs e)
         {
             Close();
+        }
+
+        private void InventoryControl_Load(object sender, EventArgs e)
+        {
+            LoadFields();
+        }
+
+        private void LoadFields()
+        {
+            dateBox.Value = DateTime.Now;
+
+            using (var context = new Session6Entities())
+            {
+                var getAsset = (from x in context.Assets
+                                select new { Asset = x.AssetName + " (" + x.AssetSN + ")" }).Distinct();
+                List<string> assets = new List<string>();
+                foreach (var item in getAsset)
+                {
+                    assets.Add(item.Asset);
+                }
+
+                assetBox.Items.AddRange(assets.ToArray());
+                assetBox.SelectedItem = assets.First();
+
+                var getWarehouse = (from x in context.Warehouses
+                                    select x.Name).Distinct().ToList();
+
+                warehouseBox.Items.AddRange(getWarehouse.ToArray());
+                warehouseBox.SelectedItem = getWarehouse.First();
+            }
         }
     }
 }
